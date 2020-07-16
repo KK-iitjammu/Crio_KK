b7be62bb368beec0d47aca1c64753104e5789194
CrioUser <crio-user@criodo.com>
2020-07-16 19:30:29 +0530

    Remove validation code from initial handlers

--- android_client_handler.py
+++ android_client_handler.py
@@ -21,25 +21,3 @@ def change_user_country_android(user_name, user_country):
     # ...
     # Android specific code
     update_user_country(user_name, user_country)
-
-
-# This function is called by Android app eventually.
-def change_user_language_android(user_name, user_country, user_language):
-    # Some Android specific code goes here.
-    # Android specific code
-    # ...
-    # ...
-    # Android specific code
-
-    # Do some basic validation of whether this language is spoken in the country.
-    # It is given that users in USA can speak only English or Spanish,
-    # and Indians can speak only English or Hindi.
-    if (user_country == 'COUNTRY_INDIA'
-            and (user_language == 'LANGUAGE_HINDI' or user_language == 'LANGUAGE_ENGLISH')
-        or user_country == 'COUNTRY_USA'
-            and (user_language == 'LANGUAGE_HINDI' or user_language == 'LANGUAGE_ENGLISH')):
-        update_user_language(user_name, user_language)
-    else:
-        raise Exception('Invalid country/language combination')
-
-
--- ios_client_handler.py
+++ ios_client_handler.py
@@ -20,20 +20,3 @@ def change_user_country_ios(username, country):
     # ...
     # iOS specific code
     update_user_country(username, country)
-
-
-# This function is called by iOS app eventually.
-def change_user_language_ios(username, country, language):
-    # Some iOS specific code goes here.
-    # iOS specific code
-    # ...
-    # iOS specific code
-
-    # Do some basic validation of whether this language is spoken in the country.
-    # It is given that users in USA can speak only English or Spanish,
-    # and Indians can speak only English or Hindi.
-    if (country == 'COUNTRY_USA' and (language == 'LANGUAGE_SPANISH' or language == 'LANGUAGE_ENGLISH')
-        or country == 'COUNTRY_INDIA' and (language == 'LANGUAGE_HINDI' or language == 'LANGUAGE_ENGLISH')):
-        update_user_language(username, language)
-    else:
-        raise Exception('Invalid country/language combination')
